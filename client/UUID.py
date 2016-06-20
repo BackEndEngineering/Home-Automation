a90bf13d7c0655e2d242c987eacb4a6b07c724a9
David C. Villarreal <redhulk@rocketmail.com>
2016-06-20 15:50:59 -0500

    add UUID.py and update gitignore on UID

--- client/UUID.py
+++ client/UUID.py
@@ -5,6 +5,7 @@ import sys
 try:
     uid_file = open('UID', encoding='utf-8')
     print(uid_file.read())
+    uid_file.close()
     sys.exit()
 except FileNotFoundError:
     pass
@@ -13,4 +14,13 @@ headers = {'Authorization': 'Token '+ TOKEN}
 controller = {}
 response = requests.post('http://guitron.herokuapp.com/api/Controller/', json=CONFIG, headers=headers)
 print(response.status_code)
-print(response.json())
+#print(response.json())
+controller = response.json()
+
+
+try:
+    with open('UID', mode='w', encoding='utf-8') as uid_file:
+        uid_file.write(controller['uuid'])
+
+except FileNotFoundError:
+    pass
