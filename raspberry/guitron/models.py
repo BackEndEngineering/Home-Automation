60c37e9bde6dbe274616a512104bc85ea489135e
David C. Villarreal <redhulk@rocketmail.com>
2016-06-21 15:00:02 -0500

    change models.py on controller

--- raspberry/guitron/models.py
+++ raspberry/guitron/models.py
@@ -32,7 +32,7 @@ class Controller(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=50)
     user = models.CharField(max_length=50)
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    uuid = models.UUIDField(unique=True, null=False, default=uuid.uuid4, editable=False)
 
     def __str__(self):
         return str(self.id)
